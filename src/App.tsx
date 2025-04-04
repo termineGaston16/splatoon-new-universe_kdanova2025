c8ee10afab4e3ec4e450f6b7f6a9e4bca55bee54
kda/nova <gastontermine16@gmail.com>
2025-04-04 17:45:23 -0300

    Primer Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
+    )
+}
