337c91c67141f2d35fc57a18ff6f0b915f459786
kda/nova <gastontermine16@gmail.com>
2025-04-06 16:59:10 -0300

    test Offline verificado correctamente

---
+++ src/UI/Presentation/Components/Offline.test.tsx
@@ -0,0 +1,75 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import Offline from "./Offline"
+
+describe('Offline', () => {
+    beforeEach(() => {
+        render(<Offline />)
+
+    })
+    afterEach(() => {
+        vi.clearAllMocks();
+    })
+
+    it('Renderizar el cartel de alerta', () => {
+        const sign = screen.getByRole('img', { name: /alerta|mensaje|notificación/i });
+        expect(sign).toBeInTheDocument();
+    })
+
+    it('Renderizar el mensaje de alerta', () => {
+        const alert = screen.getByText(/conexión|perdida|perdido|internet/i)
+        expect(alert).toBeInTheDocument();
+    })
+
+    it('Renderizar el botón de recargar página', () => {
+        const reloadBtn = screen.getByRole('button', { name: /reintentar|recargar|cargar/i })
+        expect(reloadBtn).toBeInTheDocument();
+    })
+
+    // Un espía te permite verificar que cuando ocurre una acción (como un clic), se ejecuta una función específica.
+    it('Debe recargar la página al hacer click en el botón', () => {
+        const restore = mockReload();
+
+        const reloadBtn = screen.getByRole('button', { name: /reintentar|recargar|cargar/i });
+        fireEvent.click(reloadBtn);
+        expect(window.location.reload).toHaveBeenCalled();
+
+        restore();
+    });
+
+
+    it('Recarga la página automáticamente al volver el internet', () => {
+        vi.useFakeTimers(); // <-- Activar los timers falsos.
+
+        const restore = mockReload();
+
+        // Simulamos el evento 'online' (cuando vuelve la conexión)
+        window.dispatchEvent(new Event('online'));
+
+        // Avanzamos el tiempo para que se ejecute el setTimeout
+        vi.runAllTimers();
+
+        expect(window.location.reload).toHaveBeenCalled();
+
+        restore();
+        vi.useRealTimers(); // Limpiar al final
+    });
+})
+
+
+function mockReload() {
+    const originalLocation = window.location;
+    Object.defineProperty(window, 'location', {
+        value: {
+            ...originalLocation,
+            reload: vi.fn(),
+        },
+        writable: true,
+    });
+    return () => {
+        Object.defineProperty(window, 'location', {
+            value: originalLocation,
+        });
+    };
+}
+
+
---
+++ src/UI/Presentation/Components/Offline.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+
+export default function Offline() {
+
+    //Esto detecta si hay internet y recarga solo la página
+    useEffect(() => {
+        const handleOnline = () => {
+            setTimeout(() => window.location.reload(), 1000);
+        };
+
+        window.addEventListener('online', handleOnline);
+
+        return () => window.removeEventListener('online', handleOnline);
+    }, [])
+
+    return (
+        <section>
+            <img
+                loading="lazy"
+                src="public\pictures\details\alert-message.png"
+                alt="Alerta del estado Offline" />
+            <p>
+                <span>¡Oh no!</span>
+                Parece que la conexión a internet se ha perdido.
+            </p>
+            <button
+                onClick={() => window.location.reload()}
+                type="button">Reintentar</button>
+        </section>
+    )
+}
