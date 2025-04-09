5e28941578e0feea598e89bfec7925a1c8c93c04
kda/nova <gastontermine16@gmail.com>
2025-04-09 19:16:30 -0300

    test MainCovers implementado

---
+++ src/GeneralProfile/Presentation/Components/MainCovers.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from "@testing-library/react";
+import MainCovers from "./MainCovers";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+describe('MainCovers', () => {
+    it('Renderizar las 4 imagenes que se le pasan por promt', () => {
+        const alt1 = 'imagen 1'
+        const alt2 = 'imagen 3'
+        const alt3 = 'imagen 3'
+        const alt4 = 'imagen 4'
+
+        render(
+            <MemoryRouter>
+                <MainCovers
+                    alt1={alt1}
+                    alt2={alt2}
+                    alt3={alt3}
+                    alt4={alt4}
+                    url1=""
+                    url2=""
+                    url3=""
+                    url4=""
+                    ariaLabelCoverOne=""
+                    ariaLabelCoverTwo=""
+                    linkCoverOne=""
+                    linkCoverTwo=""
+                />
+            </MemoryRouter>
+        )
+
+        const images = screen.getAllByRole('img', {
+            name: /image/i
+        })
+        expect(images).toHaveLength(4)
+    })
+
+    it('La primera portada individual debe redireccionar al link pasado por promt', async () => {
+        const linkCover = 'primera-ruta';
+
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <MainCovers
+                    alt1={'imagen de redirección 1'}
+                    alt2={''}
+                    alt3={''}
+                    alt4={''}
+                    url1=""
+                    url2=""
+                    url3=""
+                    url4=""
+                    linkCoverOne={linkCover}
+                    ariaLabelCoverOne={'Acceder al perfil Splatoon desde la página principal'}
+                    ariaLabelCoverTwo=""
+                    linkCoverTwo=""
+                />
+
+                <Routes>
+                    <Route
+                        path="/primera-ruta"
+                        element={
+                            <div>
+                                Sección 1
+                            </div>
+                        }
+                    />
+                </Routes>
+            </MemoryRouter>
+        )
+
+        const img = screen.getByRole('img', {
+            name: 'imagen de redirección 1'
+        })
+
+        expect(img).toBeInTheDocument();
+
+        const individualCoverOne = screen.getByRole('link', {
+            name: 'Acceder al perfil Splatoon desde la página principal'
+        });
+
+        await userEvent.setup().click(individualCoverOne);
+
+        expect(
+            screen.getByText('Sección 1')
+        ).toBeInTheDocument();
+
+    });
+});
---
+++ src/GeneralProfile/Presentation/Components/MainCovers.tsx
@@ -0,0 +1,57 @@
+import IndividualCover from "./IndividualCover";
+
+interface Props {
+    alt1: string,
+    alt2: string,
+    alt3: string,
+    alt4: string,
+    url1: string,
+    url2: string,
+    url3: string,
+    url4: string,
+    linkCoverOne: string,
+    ariaLabelCoverOne: string,
+    linkCoverTwo: string,
+    ariaLabelCoverTwo: string
+}
+
+const MainCovers = (
+    {
+        alt1,
+        alt2,
+        alt3,
+        alt4,
+        url1,
+        url2,
+        url3,
+        url4,
+        linkCoverOne,
+        ariaLabelCoverOne,
+        linkCoverTwo,
+        ariaLabelCoverTwo
+    }: Props
+) => {
+    return (
+        <nav>
+            <IndividualCover
+                alt1={alt1}
+                alt2={alt2}
+                ariaLabel={ariaLabelCoverOne}
+                path={linkCoverOne}
+                url1={url1}
+                url2={url2}
+            />
+
+            <IndividualCover
+                alt1={alt3}
+                alt2={alt4}
+                ariaLabel={ariaLabelCoverTwo}
+                path={linkCoverTwo}
+                url1={url3}
+                url2={url4}
+            />
+        </nav>
+    )
+}
+
+export default MainCovers;
